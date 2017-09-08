ec050d596080523db1ed910a0d672bc2bb15a870
Fredrik Liljedahl <fliljeda@kth.se>
2017-09-08 11:30:59 +0200

    Done

--- hmm1/Hmm1.java
+++ hmm1/Hmm1.java
@@ -8,26 +8,57 @@ public class Hmm1 {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         // [y][x]
-        double[][] stateMatrix, obsMatrix, initMatrix;
+        double[][] stateMatrix, obsMatrix;
+        double[] initMatrix;
         int[] sequence;
+        double[] alpha, alphaPrev;
+        double[] alphaTemp;
 
         public static void main(String[] args){
             new Hmm1();
         }
 
-
         public Hmm1(){
             parseMatrices();
-            // doTheThing();
+            doTheThing();
             writeOutput();
         }
 
-
         public void writeOutput(){
-            System.out.println("sequence: " + Arrays.toString(sequence));
+            double sum = 0;
+            for(int i = 0; i < alpha.length; i++){
+                sum += alpha[i];
+            }
+            System.out.println(sum);
         }
 
         public void doTheThing(){
+            //Initialize pi* b(O_1)
+            int alphalength = initMatrix.length;
+            alpha = new double[alphalength];
+            alphaPrev = new double[alphalength];
+
+            for(int i = 0; i < alphalength; i++){
+                alpha[i] = initMatrix[i] * obsMatrix[i][sequence[0]];
+            }
+
+            //Looping (sum of alpha_t-1 [i] + a[i][j]) * b_i(O_t)
+            int sequenceLength = sequence.length;
+
+            for(int t = 1; t < sequenceLength; t++){
+                alphaPrev = alpha;
+                alpha = new double[alphalength];
+                for(int j = 0; j < alphalength; j++){
+                    double currentsum = 0;
+                    for(int i = 0; i < alphalength; i++){
+                        currentsum += alphaPrev[i] * stateMatrix[i][j];
+                    }
+                    int observation = sequence[t];
+                    alpha[j] = currentsum * obsMatrix[j][observation];
+                }
+
+            }
+
 
         }
 
@@ -55,11 +86,9 @@ public class Hmm1 {
                              }
                          }
                      }else if(id == 2){
-                         initMatrix = new double[ysize][xsize];
-                         for (int y = 0; y < ysize; y++){
-                             for(int x = 0; x < xsize; x++){
-                                  initMatrix[y][x] = Double.parseDouble(tokens[(y*xsize) + x + 2]);
-                             }
+                         initMatrix = new double[xsize];
+                         for(int x = 0; x < xsize; x++){
+                              initMatrix[x] = Double.parseDouble(tokens[x + 2]);
                          }
                      }else if(id == 3){
                          int sequenceSize = Integer.parseInt(tokens[0]);
